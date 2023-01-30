ed12ebcb422981f2b14cf983747d10e11151348d
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2023-01-30 07:01:00 -0800

    Colocando texto no Signos

--- Signos aula/ViewController.swift
+++ Signos aula/ViewController.swift
@@ -29,18 +29,18 @@ class ViewController: UITableViewController {
         signos.append("Peixes")
         
         //configura significados
-        significadoSignos.append("O ariano é uma pessoa cheia de energia e entusiasmo. Pioneiro e eventureiro, lhe encantam as metas, a liberdade e as idéias novas.")
-        significadoSignos.append("Zeloso e possessivo, um cauro pode tender a ser inflexível e ressentido. As vezes os Touro pecam de ser cobiçosos e de permitir-se tudo.")
-        significadoSignos.append("2 - ..")
-        significadoSignos.append("3 - ..")
-        significadoSignos.append("4 - ..")
-        significadoSignos.append("5 - ..")
-        significadoSignos.append("6 - ..")
-        significadoSignos.append("7 - ..")
-        significadoSignos.append("9 - ..")
-        significadoSignos.append("10 - ..")
-        significadoSignos.append("11 - ..")
-        significadoSignos.append("12 - ..")
+        significadoSignos.append("Quem tem sol em Áries costuma viver no agora; além de possuir um grande prazer em existir, ação e decisão também podem ser algumas de suas marcas. A liderança é uma característica forte no signo, que pode ser usada para o empreendedorismo.")
+        significadoSignos.append("Quem tem sol em Touro costuma não abrir mão do que traz conforto e bem-estar, mas pode ter a possessividade em evidência. O signo preza pela beleza física e pela gula. A satisfação de Touro vem do contato com todos os cinco sentidos.")
+        significadoSignos.append("Quem tem sol em Gêmeos costuma apreciar novas ideias, novos lugares e novas pessoas. Gêmeos tem carisma de sobra e tende a fazer amigos com muita facilidade. A superficialidade e a inconsequência também podem ser algumas das marcas do signo.")
+        significadoSignos.append("Quem tem sol em Câncer costuma buscar o que lhe traz segurança. O signo é maternal e emotivo e tem uma proximidade com o apego ao passado. Câncer costuma ter a intuição acentuada, sentir as coisas no ar e captar tudo logo no primeiro olhar.")
+        significadoSignos.append("Quem tem sol em Leão costuma amar plateias e aplausos. O signo tem a coragem e o humor como marcas registradas, mas também o orgulho e o autoritarismo. Leão geralmente valoriza as oportunidades e aproveita cada momento, sabe amar e ter generosidade.")
+        significadoSignos.append("Quem tem sol em Virgem costuma buscar constantemente a perfeição, tudo sempre poderia estar um pouco melhor. Signo observador, que costuma identificar o detalhe que ninguém mais percebe. Virgem tem praticidade e organização, mas também pode ser frio e cético.")
+        significadoSignos.append("Quem tem sol em Libra costuma buscar sempre o equilíbrio e conviver com a indecisão. Regido por Vênus, o signo tem uma aproximação natural com as artes. Libra costuma ter ética, mas também pode ter vaidade em excesso e comportamentos considerados de “mau gosto”.")
+        significadoSignos.append("Quem tem sol em Escorpião costuma ter muita perspicácia, não é a toa que são naturalmente detetives. O sexto sentido do signo alerta para possíveis “perigos” logo de cara. Escorpião tem uma capacidade de regeneração única, mas também pode ter muitos ciúmes.")
+        significadoSignos.append("Quem tem sol em Sagitário costuma ser viajante, sua casa é o mundo inteiro. O signo viaja muito também em sua própria mente. Não tente aprisionar Sagitário de nenhuma forma, ele não aceita limites. Otimista e jovial, pode ter exageros e uma dose de deboche.")
+        significadoSignos.append("Quem tem sol em Capricórnio costuma ter seus propósitos e metas bem definidos. Signo de praticidade que segue as regras do jogo tem total noção dos seus limites. Capricórnio é maduro e bem humorado, mas também pode ser frio nas relações e um tanto pessimista.")
+        significadoSignos.append("Quem tem sol em Aquário costuma ter a amizade como cartão de visita e ter uma conexão intensa com a liberdade. Signo idealista, tende a dar preferência aos diálogos racionais. Aquário é solidário e humano, também pode ser radical e bem “do contra”.")
+        significadoSignos.append("Quem tem sol em Peixes costuma entender tudo sem precisar de muitas palavras, possuem uma sensibilidade superior. Signo compassivo e inspirador, Peixes tende a ter uma forte conexão com a espiritualidade, mas pode se iludir com certa facilidade.")
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -60,7 +60,18 @@ class ViewController: UITableViewController {
         return celuls
     }
     
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let alertaController = UIAlertController(title: "Siginificado do signo", message: significadoSignos[indexPath.row], preferredStyle: .alert )
+        
+        let acaoConfirmar = UIAlertAction(title: "ok", style: .default, handler: nil )
+        
+        alertaController.addAction(acaoConfirmar)
+        
+        present( alertaController, animated: true, completion: nil )
+    }
 
 }
 
